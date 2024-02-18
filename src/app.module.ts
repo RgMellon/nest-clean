e478201e607f63244e6f86606280f9bac4de5b88
Renan <renan_gian@hotmail.com>
2024-02-18 15:52:12 -0300

    feat: config jwt-token, and use to inject on auth module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,11 @@ import { PrismaService } from './prisma/prisma.service'
 import { CreateAccountController } from './controllers/create-account.controller'
 import { ConfigModule } from '@nestjs/config'
 import { envSchema } from './env'
+import { AuthModule } from './auth/auth.module'
 
 @Module({
   imports: [
+    AuthModule,
     ConfigModule.forRoot({
       validate: (env) => envSchema.parse(env),
       isGlobal: true,
