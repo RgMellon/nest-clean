f96012d2e6d8d503fe6551666c5fe3970bb3101d
Renan <renan_gian@hotmail.com>
2024-02-21 09:30:16 -0300

    feat: create authentication flux

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,8 @@ import { CreateAccountController } from './controllers/create-account.controller
 import { ConfigModule } from '@nestjs/config'
 import { envSchema } from './env'
 import { AuthModule } from './auth/auth.module'
+import { AuthenticationController } from './controllers/authenticate-controller'
+import { CreateQuestionController } from './controllers/create-question.controlle'
 
 @Module({
   imports: [
@@ -13,7 +15,11 @@ import { AuthModule } from './auth/auth.module'
       isGlobal: true,
     }),
   ],
-  controllers: [CreateAccountController],
+  controllers: [
+    CreateAccountController,
+    AuthenticationController,
+    CreateQuestionController,
+  ],
   providers: [PrismaService],
 })
 export class AppModule {}
